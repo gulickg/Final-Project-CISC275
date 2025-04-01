d37f9b0a225bee70d1ad0b04fbd75c83605e0557
Alexis Vogt <alexisv@udel.edu>
2025-04-01 17:51:14 -0400

    Started laying out homepage

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -4,6 +4,10 @@ import './Homepage.css'
 export function Homepage():React.JSX.Element{
     return(<div>
         <h1 id='title'>Welcome to the Career Helpi!</h1>
+        <div id='content-box'>
+            <div className='quiz' id='basic-info'>Basic Quiz</div>
+            <div className='quiz' id='detailed-info'>Detailed Quiz</div>
+        </div>
     </div>);
 
 }
