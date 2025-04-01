9f63bdffb28a33f617fb74e3ca323538e7359b63
Alexis Vogt <alexisv@udel.edu>
2025-04-01 19:35:14 -0400

    Added description box format

--- src/Components/DescriptionBox.tsx
+++ src/Components/DescriptionBox.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 import './Homepage.css'
+import './DescriptionBox.css'
 
 interface DescriptionProps{
     questionType:string;
     questionDescription: string;
 }
 
-export function Description({questionType}:DescriptionProps, {questionDescription}:DescriptionProps):React.JSX.Element{
-    return(<div>
-        <div>{questionType}</div>
-        <div>{questionDescription}</div>
+export function Description({questionType, questionDescription}:DescriptionProps):React.JSX.Element{
+    return(<div id='description-box'>
+        <p id='question-type-title'>{questionType}</p>
+        <p id='desc'>{questionDescription}</p>
+        <div id='button-box'>
+            <Button id="continue-button">Continue to Quiz</Button>
+        </div>
     </div>);
 
 }
--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -3,13 +3,16 @@ import './Homepage.css'
 import { Description } from './DescriptionBox';
 
 export function Homepage():React.JSX.Element{
+
     return(<div>
         <h1 id='title'>Welcome to the Career Helpi!</h1>
         <div id='content-box'>
             <div className='quiz' id='basic-info'>
-                <Description questionType={'Basic'} questionDescription={"TESTTTT"}></Description>
+                <Description questionType={'Basic'} questionDescription={'Description'}></Description>
             </div>
-            <div className='quiz' id='detailed-info'></div>
+            <div className='quiz' id='detailed-info'>
+            <Description questionType={'Detailed'} questionDescription={'Description'}></Description>
+            </div>
         </div>
     </div>);
 
