42f95f465edf37af20f5009d4bcb7320d492bd48
Alexis Vogt <alexisv@udel.edu>
2025-04-01 21:16:21 -0400

    Added progress bar to detailed question page

--- src/Components/DetailedQuestions.tsx
+++ src/Components/DetailedQuestions.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './DetailedQuestions.css'
+
+interface ProgressBarProps{
+    width: number
+}
 
 export function DetailedQuestions():React.JSX.Element{
-    return(<></>);
+    const [questionsCompleted, setQuestionsCompleted] = useState<number>(0);
+        const totalQuestions = 7;
+    
+        const progressPercent = Math.ceil(questionsCompleted / totalQuestions * 100)
+        const progressBarSize = progressPercent / 100 * 185 > 185 ? 185 : Math.ceil(progressPercent / 100 * 185);
+    
+    
+        return(<div id='detailed-questions-page'>
+            <h1 id='title'>Detailed Quiz Questions</h1>
+            <div>Progress:</div>
+            <div id='detailed-progress-bar-box'>
+                <div id='detailed-progress-bar' style={{ width: progressBarSize}}></div>
+            </div>
+            {progressPercent}%
+        </div>);
 
 }
