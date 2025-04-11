7fe714e4314347939b87c1b74fda20a7c788d884
Alexis Vogt <alexisv@udel.edu>
2025-04-10 20:50:02 -0400

    Added textbox input template for detailed quiz

--- src/Components/DetailedQuestions.tsx
+++ src/Components/DetailedQuestions.tsx
@@ -3,6 +3,7 @@ import './DetailedQuestions.css'
 import './BasicQuestions.css'
 import './Question-Templates/RadioButtonsQuestion'
 import './Question-Templates/TextInputQuestion'
+import { TextInputQuestion } from './Question-Templates/TextInputQuestion'
 
 
 /*
@@ -12,12 +13,12 @@ This is the detailed questions page
 
 export function DetailedQuestions():React.JSX.Element{
     const [questionsCompleted, setQuestionsCompleted] = useState<number>(0);
-        const totalQuestions = 7;
+    const totalQuestions = 7;
     
     // find the percent of questions completed
-    let progressPercent:number = Math.ceil(questionsCompleted / totalQuestions * 100)
-    let progressBarSize = progressPercent / 100 * 185 > 185 ? 185 : Math.ceil(progressPercent / 100 * 185);
-    setQuestionsCompleted(1);
+    // let progressPercent:number = Math.ceil(questionsCompleted / totalQuestions * 100)
+    // let progressBarSize = progressPercent / 100 * 185 > 185 ? 185 : Math.ceil(progressPercent / 100 * 185);
+    // setQuestionsCompleted(1);
     // pass to each question: updates questions completed
     // function addCompleted(){
     //     setQuestionsCompleted(questionsCompleted + 1);
@@ -30,10 +31,11 @@ export function DetailedQuestions():React.JSX.Element{
         return(<div id='detailed-questions-page'>
             <h1 id='title'>Detailed Quiz Questions</h1>
             <div>Progress:</div>
-            <div id='detailed-progress-bar-box'>
+            {/* <div id='detailed-progress-bar-box'>
                 <div id='detailed-progress-bar' style={{ width: progressBarSize}}></div>
             </div>
-            {progressPercent}%
+            {progressPercent}% */}
+            <TextInputQuestion question='example' qNumber={1}></TextInputQuestion>
         </div>);
 
 }
--- src/Components/Question-Templates/TextInputQuestion.tsx
+++ src/Components/Question-Templates/TextInputQuestion.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-// import { Form, Button} from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Form } from 'react-bootstrap'
+import questionMark from './question-mark.png'
 import './Questions.css'
 
 /*
@@ -9,8 +10,35 @@ template for text input question
 
 interface TextQuestionProps {
     question: string;
+    qNumber: number
 }
 
-export function TextInputQuestion({question}: TextQuestionProps):React.JSX.Element{
-    return(<div className='text-input-question'></div>)
+export function TextInputQuestion({question, qNumber}: TextQuestionProps):React.JSX.Element{
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event:React.ChangeEvent<HTMLTextAreaElement>){
+        setAnswer(event.target.value);
+    }
+
+    return(<div className='text-input-question'>
+        <div className='question-number'>
+            Question 1:
+            <img id='more-info' src={questionMark} alt='question mark'></img>
+        </div>
+        
+        <div>
+            <Form.Group controlId='text-answer'>
+                <Form.Label className='question-text'>
+                    {question}
+                </Form.Label>
+                <Form.Control
+                as='textarea'
+                rows={3}
+                value={answer}
+                onChange={updateAnswer}>
+
+                </Form.Control>
+            </Form.Group>
+        </div>
+    </div>)
 }
