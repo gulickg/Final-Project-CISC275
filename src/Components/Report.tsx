0209945670ed14f35d1ffce3b5a1fecbdfd5d8ea
brynnmal <brynnmal@udel.edu>
2025-04-23 15:24:59 -0400

    some work on report done

--- src/Components/Report.tsx
+++ src/Components/Report.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
 
-/*
-This is where the report template will go.
-*/
-
-
 export function Report():React.JSX.Element{
-    let careerSuggestion: string = ""; //this needs to come from the AI
-    let answerBreakdown: string = ""; //list of questions linked to the users answers
+    //let careerSuggestion: string = ""; //this needs to come from the AI
+    //let answerBreakdown: string = ""; //list of questions linked to the users answers
     return(
     <div id='report'>
         <h1 id='title'>Quiz Results</h1>
-        <div id='quiz output'>
-                <h2>Career Suggestion</h2>
-                <p>{careerSuggestion}</p>
-                <h2>Answer Breakdown</h2>
-                <p>{answerBreakdown}</p>
-                <h2>Still stuck? Follow up with ChatGPT.</h2>
-                {//how to do AI Integration
-}
+        <div id='container'>
+            <div id='box'>
+                <div id = 'careerSuggestion'>
+                    {/* this comes from the ai */}
+                </div>
+                <div id = 'careerDescription'>
+                    {/* this comes from ai */}
+                </div>
             </div>
+            <div id='box'>
+                <div id='answerBreakdown'>
+                    {/*this comes from somewhere else*/}
+                </div>
+                <div id='ai'>
+                    <h2>Still stuck? Follow up with ChatGPT.</h2>
+                    {/*ai shit*/}
+                </div>
+            </div>
+        </div>
             <div id="shareButton">
                 <button>Share your results</button>
             </div>
