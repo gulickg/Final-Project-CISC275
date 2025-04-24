99b387608a5393567920684d1edc0baadec38cb3
brynnmal <brynnmal@udel.edu>
2025-04-24 16:07:55 -0400

    report bug fixed

--- src/Components/Report.tsx
+++ src/Components/Report.tsx
@@ -3,16 +3,26 @@ import React from 'react'
 export function Report(suggestion: string, description: string, breakdown: string, type: string):React.JSX.Element{
     return(
     <div id='report'>
-        <h1 id='title'>Quiz Results</h1>
-        <div id='quiz output'>
-                <h2>Career Suggestion</h2>
-                <p>{careerSuggestion}</p>
-                <h2>Answer Breakdown</h2>
-                <p>{answerBreakdown}</p>
-                <h2>Still stuck? Follow up with ChatGPT.</h2>
-                {//how to do AI Integration
-}
+        <h1 id='title'>{type} Quiz Results</h1>
+        <div id='container'>
+            <div id='box'>
+                <div id = 'careerSuggestion'>
+                    {suggestion}
+                </div>
+                <div id = 'careerDescription'>
+                    {description}
+                </div>
             </div>
+            <div id='box'>
+                <div id='answerBreakdown'>
+                    {breakdown}
+                </div>
+                <div id='ai'>
+                    <h2>Still stuck? Follow up with ChatGPT.</h2>
+                    {/*ai stuff - do we get rid of?*/}
+                </div>
+            </div>
+        </div>
             <div id="shareButton">
                 <button>Share your results</button>
             </div>
