270d370136bf968669753ea332b0bd1fb2867726
Grace <grace.c.gulick@gmail.com>
2025-05-06 10:30:01 -0400

    Trying another input

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -40,15 +40,13 @@ export function Homepage({setPage}: HomepageProps):React.JSX.Element{
                 <div className='quiz' id='basic-info'>
                     <Description questionType={'Basic'} questionDescription={basicDescription} setPage={setPage}></Description>
                 </div>
-                <div className="w-full max-w-xs p-5 bg-white rounded-lg font-mono">
-                    <label className="block #024932 text-sm font-bold mb-2" htmlFor="unique-input"
-                            >Enter OpenAI API key here</label>
-                    <input
-                        className="text-sm custom-input w-full px-4 py-2 border #266d56 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:#266d56 hover:shadow-lg hover:#266d56 bg-gray-100"
-                        placeholder="API key"
-                        type="text"
-                        id="api"
-                    />
+                <div className="group">
+                    <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
+                        <g>
+                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                        </g>
+                    </svg>
+                    <input placeholder="Search" type="search" className="input"/>
                 </div>
                 <div id='hpc'></div>
                 <div className='quiz' id='detailed-info'>
